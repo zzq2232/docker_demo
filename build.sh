c9cbda1480adbbf30332f95a9833312f71696755
zzq2232 <86610271+zzq2232@users.noreply.github.com>
2022-12-02 15:46:15 +0800

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,3 @@
+echo "运行容器python执行自动化"  #输出日志
+#-w=WORKSPACE：指定workspace #--volumes-from=jenkins_save01：将jenkins容器中的workspace映射到python容器中，此时jenkins中git拉下来的代码就会被映射进去 docker run --rm -w=WORKSPACE --volumes-from=jenkins_save01 python3
+echo "python执行自动化执行成功"
